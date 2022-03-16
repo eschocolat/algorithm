3ede736fe0fc0934d1c1c32005aebacc1899ebeb
eschocolat <eschocolat.lee@gmail.com>
2022-03-17 01:05:09 +0900

    LeetCode: lru-cache

---
+++ leetcode/146/solution.py
@@ -0,0 +1,21 @@
+from collections import OrderedDict
+
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = OrderedDict()
+
+    def get(self, key: int) -> int:
+        value = self.cache.get(key, -1)
+        if value != -1:
+            self.cache.move_to_end(key)
+        return value
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        elif len(self.cache) >= self.capacity:
+            self.cache.popitem(last=False)
+        self.cache[key] = value
